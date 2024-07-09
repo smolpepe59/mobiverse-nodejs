15a208bfd05c0e736a6862c3a2dab3c76abe6b3d
smolpepe59 <139532388+smolpepe59@users.noreply.github.com>
2024-07-09 17:12:36 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,13 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const statusFilePath = path.join(__dirname, 'status.txt');
+
+fs.writeFile(statusFilePath, '', (err) => {
+    if (err) {
+        console.error(`Error clearing the file: ${err}`);
+    } else {
+        console.log('Status file content cleared.');
+    }
+});
