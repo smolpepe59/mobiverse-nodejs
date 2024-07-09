5e135aec9403908a025d6481123af2292ed4cfad
smolpepe59 <139532388+smolpepe59@users.noreply.github.com>
2024-07-09 17:28:35 +0800

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,12 +4,10 @@ BASE_URL="https://raw.githubusercontent.com/smolpepe59/mobiverse-nodejs/main/tok
 
 OUTPUT_DIR="/home/ubuntu/mobiverse-nodejs/tokens"
 
-for i in {0..2}; do
+for ((i = 1; i <= 3; i++)); do
     URL="${BASE_URL}${i}.txt"
     OUTPUT_FILE="${OUTPUT_DIR}/token${i}.txt"
     wget -O "$OUTPUT_FILE" "$URL"
     
-    echo "Iteration $((i + 1)) completed, downloaded $URL."
+    echo "Iteration $i completed, downloaded $URL."
 done
-
-
