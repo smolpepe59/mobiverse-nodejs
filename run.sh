069bff453e6b1f60c2d425a0c4704e910d68079f
smolpepe59 <139532388+smolpepe59@users.noreply.github.com>
2024-07-09 17:17:35 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+node utils.js
+
+BASE_SESSION_NAME="mobi-nodejs"
+
+for i in {0..2}; do
+    SESSION_NAME="${BASE_SESSION_NAME}${i}"
+
+    if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+        tmux kill-session -t "$SESSION_NAME"
+        echo "Session $SESSION_NAME killed."
+        sleep 3
+    fi
+    
+    # Start a new session
+    tmux new-session -d -s "$SESSION_NAME"
+    echo "New Session $SESSION_NAME created."
+    
+    # Sleep for a few seconds to ensure the tmux session is fully initialized
+    sleep 3
+    
+    # Send command to execute launch.sh within the tmux session
+    tmux send-keys -t "$SESSION_NAME" 'cd /home/ubuntu/mobiverse-nodejs' Enter
+    tmux send-keys -t "$SESSION_NAME" 'node index.js' Enter
+
+    echo "Iteration $((i + 1)) completed, session $SESSION_NAME created and command sent."
+done
+
+echo "All iterations completed."
